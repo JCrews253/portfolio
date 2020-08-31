a69fdd44f9901e9f7dd044770bb846ee419cd38c
JCrews253 <jcrews206@gmail.com>
2020-08-31 13:57:03 -0700

    updated about me

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import './About.css'
-import profilePicture from '../../images/profile-picture.jpg'
+import React from "react";
+import "./About.css";
+import profilePicture from "../../images/profile-picture.jpg";
 
 const About = () => {
-    return(
-        <div className='about'>
-            <img className='about__profilePicture' src={profilePicture}/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique minima, asperiores est quod vitae saepe nostrum nam aliquam quae adipisci voluptatum eum libero nulla, perferendis, eos esse a quaerat exercitationem!</p>
-        </div>
-    )
-}
+  return (
+    <div className="about">
+      <img className="about__profilePicture" src={profilePicture} />
+      <p>
+        {" "}
+        Hello! My name is James Crews and I am an aspiring self-taught software
+        engineer. Over the past couple years i have been teaching myself how to
+        code, with most of my experience being in C# and Typescript/Javascript,
+        specifically in the React framework. I have a heavy interest in
+        algorithmic programming and a love solving complex problems. A few of my
+        projects showing my progress can be found on this website.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
